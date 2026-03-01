d9bf370c71a102ac4c3b2da66866b66ed43ae096
gauravpatels058-lab <gauravpatels058@gmail.com>
2026-03-01 17:23:22 +0530

    Add files via upload

---
+++ index1.js
@@ -0,0 +1,7 @@
+// Profile pic ko select karna 
+const profilePic = document.getElementById('profilePic');
+
+//Click event add karna
+profilePic.addEventListener('click',function(){
+    alert('Welcome to my Portfolio! I am Gaurav, an aspiring Full Stack Developer.');
+});
