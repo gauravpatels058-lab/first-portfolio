6a45a07aec9076ece0b1aff1703abe95f3d8af52
gauravpatels058-lab <gauravpatels058@gmail.com>
2026-03-30 07:44:13 +0530

    Update index1.js

--- index1.js
+++ index1.js
@@ -1,7 +1,41 @@
-// Profile pic ko select karna 
-const profilePic = document.getElementById('profilePic');
+/// Elements select
+const menuToggle = document.getElementById("menuToggle");
+const navLinks = document.getElementById("navLinks");
 
-//Click event add karna
-profilePic.addEventListener('click',function(){
-    alert('Welcome to my Portfolio! I am Gaurav, an aspiring Full Stack Developer.');
+// Toggle menu
+if (menuToggle && navLinks) {
+    menuToggle.addEventListener("click", () => {
+        navLinks.classList.toggle("active");
+    });
+}
+
+const links = document.querySelectorAll("#navLinks a");
+
+links.forEach(link => {
+    link.addEventListener("click", () => {
+        navLinks.classList.remove("active");
+    });
+});
+
+window.addEventListener("scroll", () => {
+    const navbar = document.querySelector(".navbar");
+
+    if (window.scrollY > 50) {
+        navbar.classList.add("scrolled");
+    } else {
+        navbar.classList.remove("scrolled");
+    }
+});
+
+const reveals = document.querySelectorAll(".reveal");
+
+window.addEventListener("scroll", () => {
+    reveals.forEach((el) => {
+        const windowHeight = window.innerHeight;
+        const elementTop = el.getBoundingClientRect().top;
+
+        if (elementTop < windowHeight - 100) {
+            el.classList.add("active");
+        }
+    });
 });
